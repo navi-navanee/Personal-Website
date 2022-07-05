af3f1d6a5cbfe5b03daee11832613c03945c455f
Navaneeth Devan <navaneethdevanofficial@gmail.com>
2022-07-05 15:58:39 +0530

    general styling is donw

--- my-app/src/component/header/Header.jsx
+++ my-app/src/component/header/Header.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 import './header.css'
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+       <div className='container header__container'>
+        <h5>Helloo I'm</h5>
+        <h1>Navaneeth Devan</h1>
+        <h5 className='text-light'>Fullstack developer</h5>
+       </div>
+    </header>
   )
 }
 
