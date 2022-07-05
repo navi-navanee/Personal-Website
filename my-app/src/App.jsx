138eec5847e2ddad5f78defe2ffbff34d8955e69
Navaneeth Devan <navaneethdevanofficial@gmail.com>
2022-07-05 15:10:08 +0530

    personal website component folder are created

---
+++ my-app/src/App.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import About from './component/about/About'
+import { Contact } from './component/contact/Contact'
+import Experiance from './component/experiance/Experiance'
+import Footer from './component/footer/Footer'
+import Header from './component/header/Header'
+import { Nav } from './component/nav/Nav'
+import Portfolio from './component/portfolio/Portfolio'
+import Services from './component/Services/Services'
+
+const App = () => {
+  return (
+    <>
+      <Header/>
+      <Nav/>
+      <About/>
+      <Experiance/>
+       <Services/>
+       <Portfolio/>
+       <Contact/>
+       <Footer/>
+    </>
+  )
+}
+
+export default App
---
+++ my-app/src/component/testimonials/Testimonials.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './testimonilas.css'
+const Testimonials = () => {
+  return (
+    <div>Testimonials</div>
+  )
+}
+
+export default Testimonials
